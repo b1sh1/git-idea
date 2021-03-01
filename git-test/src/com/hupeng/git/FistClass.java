4de5cd6dd45ece3890cd3b7ae7ee3badf660fdf6
hupeng <hupeng@123>
2021-03-01 16:07:38 +0800

    把本地工厂添加到git 2

--- git-test/src/com/hupeng/git/FistClass.java
+++ git-test/src/com/hupeng/git/FistClass.java
@@ -4,4 +4,9 @@ public class FistClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i = 10;
+        i += 10;
+    }
 }
